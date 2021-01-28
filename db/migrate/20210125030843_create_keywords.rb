3d00de2656e2cd804243fcbc3bd92b6e47571e4f
ferchomuri <edwinmo_1994@hotmail.com>
2021-01-27 22:13:26 -0500

    Commit Seed

--- db/migrate/20210125030843_create_keywords.rb
+++ db/migrate/20210125030843_create_keywords.rb
@@ -2,7 +2,6 @@ class CreateKeywords < ActiveRecord::Migration[6.1]
   def change
     create_table :keywords do |t|
       t.string :description
-      t.integer :category_id
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    c1 = Category.create(name:"Bebidas")
+    c2 = Category.create(name:"Legumbres")
+
+    p1 = Product.create(name:"Coca Cola",color:"Roja",size:5,price:2.40)
+    p2 = Product.create(name:"Lenteja",color:"Cafe",size:2,price:0.89)
+
+    s1 = Store.create(name:"Olivitos", street:"Av Amazonas", number:"Oe3-64", zipcode:"170204", city:"Quito", country:"Ecuador")
+    s2 = Store.create(name:"La tienda de la esquina", street:"Av Galo Lasso Plaza", number:"N3-54", zipcode:"170185", city:"Quito", country:"Ecuador")
+
+    k1 = Keyword.create(description: "Jugo")
+    k2 = Keyword.create(description: "Ensalada")
